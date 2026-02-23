56aef0a9df153d3191865aa7e7ba6e016e1b3288
fbruggem <fbruggem@proton.me>
2026-02-23 18:57:29 +0100

    added help message

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "ft_ping.h"
 
+void print_usage();
+
 int main(int ac, char **av) {
   struct address addr = {0};
   struct flags flags = {0};
   if (parse(ac, av, &addr, &flags))
     return -1;
+
+  if (flags.help.present) {
+    print_usage();
+    return 0;
+  }
 
   __uint16_t sequence = 0;
   float packets_received[MAX_ICMP_SENDS] = {0};
@@ -14,4 +21,13 @@ int main(int ac, char **av) {
   if (print_summary(&addr, sequence, packets_received))
     return -1;
   return 0;
+}
+
+void print_usage() {
+  printf("Usage: ping [OPTION...] HOST ...\n"
+         "Send ICMP ECHO_REQUEST packets to network hosts.\n\n"
+         "Options\n"
+         "      --ttl=N                specify N as time-to-live\n"
+         "      -v,                    verbose output\n"
+         "      -?,                    give this help list\n");
 }
